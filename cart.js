571d1b35af86da06b070f7831409d2e68590b1ac
Neovastor <neovastr1997@gmail.com>
2021-04-23 10:45:20 +0700

    Brian update script input to generate list

--- cart.js
+++ cart.js
@@ -1,175 +1,175 @@
-let destinasi = {
-    'Jakarta': [ 
-        { id: '1', 
-        paket: 'Silver', 
-        privilage: ['Hotel + Sarapan', 'Motor Sewaan', 'Gratis Tiket Masuk Ancol', 'Makan Malam'],
-        Harga: 500000,
-        Gambar: './toa-heftiba-a9pFSC8dTlo-unsplash.jpg'
-        },
-        { id: '2', 
-        paket: 'Gold', 
-        privilage: ['Hotel + Sarapan', 'Mobil Sewaan', 'Gratis tiket masuk ancol + dufan', 'Makan Malam'],
-        Harga: 1000000,
-        Gambar: './tiket_dufan_ancol_free_tiket_ancol__tiket_dunia_fantasi_ancol_1565117487_18f9795b_progressive.jpg'
-        },
-        { id: '3', 
-        paket: 'Platinum', 
-        privilage: ['Hotel + Sarapan', 'Mobil Sewaan', 'Gratis tiket masuk ancol + pulau seribu', 'Makan Malam'],
-        Harga: 1500000,
-        Gambar: './photo-1536392706976-e486e2ba97af.jpg'
-        },
-    ],
-    'Bandung': [
-        { id: '4', 
-        paket: 'Silver', 
-        privilage: ['Hotel + Sarapan', 'Motor Sewaan', 'Gratis tiket masuk orchid forest', 'Makan Malam'],
-        Harga: 500000,
-        Gambar: './Orchid%20Forest%20Cikole%20Lembang.jpg'
-        },
-        { id: '5', 
-        paket: 'Gold', 
-        privilage: ['Hotel + Sarapan', 'Mobil Sewaan', 'Gratis tiket masuk trans studio', 'Makan Malam'],
-        Harga: 1000000,
-        Gambar: './Spektakuler-Ayo-Jajal-Wahana-di-Trans-Studio-Cibubur-01-Finansialku.jpg'
-        },
-        { id: '6', 
-        paket: 'Platinum', 
-        privilage: ['Hotel + Sarapan', 'Mobil Sewaan', 'Sunrise trip gunung putri Lembang', 'Makan Malam'],
-        Harga: 1500000,
-        Gambar: './gunung-putri-lembang-bandung.jpg'
-        }] 
- }
+// let destinasi = {
+//     'Jakarta': [ 
+//         { id: '1', 
+//         paket: 'Silver', 
+//         privilage: ['Hotel + Sarapan', 'Motor Sewaan', 'Gratis Tiket Masuk Ancol', 'Makan Malam'],
+//         Harga: 500000,
+//         Gambar: './toa-heftiba-a9pFSC8dTlo-unsplash.jpg'
+//         },
+//         { id: '2', 
+//         paket: 'Gold', 
+//         privilage: ['Hotel + Sarapan', 'Mobil Sewaan', 'Gratis tiket masuk ancol + dufan', 'Makan Malam'],
+//         Harga: 1000000,
+//         Gambar: './tiket_dufan_ancol_free_tiket_ancol__tiket_dunia_fantasi_ancol_1565117487_18f9795b_progressive.jpg'
+//         },
+//         { id: '3', 
+//         paket: 'Platinum', 
+//         privilage: ['Hotel + Sarapan', 'Mobil Sewaan', 'Gratis tiket masuk ancol + pulau seribu', 'Makan Malam'],
+//         Harga: 1500000,
+//         Gambar: './photo-1536392706976-e486e2ba97af.jpg'
+//         },
+//     ],
+//     'Bandung': [
+//         { id: '4', 
+//         paket: 'Silver', 
+//         privilage: ['Hotel + Sarapan', 'Motor Sewaan', 'Gratis tiket masuk orchid forest', 'Makan Malam'],
+//         Harga: 500000,
+//         Gambar: './Orchid%20Forest%20Cikole%20Lembang.jpg'
+//         },
+//         { id: '5', 
+//         paket: 'Gold', 
+//         privilage: ['Hotel + Sarapan', 'Mobil Sewaan', 'Gratis tiket masuk trans studio', 'Makan Malam'],
+//         Harga: 1000000,
+//         Gambar: './Spektakuler-Ayo-Jajal-Wahana-di-Trans-Studio-Cibubur-01-Finansialku.jpg'
+//         },
+//         { id: '6', 
+//         paket: 'Platinum', 
+//         privilage: ['Hotel + Sarapan', 'Mobil Sewaan', 'Sunrise trip gunung putri Lembang', 'Makan Malam'],
+//         Harga: 1500000,
+//         Gambar: './gunung-putri-lembang-bandung.jpg'
+//         }] 
+//  }
 
-let pilihan = {}
+// let pilihan = {}
 
-// id: quantity
+// // id: quantity
 
-let productCard = document.getElementById("card")
+// let productCard = document.getElementById("card")
 
-function addPilihan(event) {
-    let buttonId = Number(event.target.getAttribute('packageId'))
-    for (let key in destinasi) {
-        for (let i = 0; i < destinasi[key].length; i++) {
-            if (destinasi[key][i].id == buttonId) {
-                if (pilihan[buttonId] == undefined) {
-                    pilihan[buttonId] = 0
-                }
-                pilihan[buttonId]++
-            }
-        }
-    }
-    generateCard(pilihan)
-    console.log(pilihan)
-}
+// function addPilihan(event) {
+//     let buttonId = Number(event.target.getAttribute('packageId'))
+//     for (let key in destinasi) {
+//         for (let i = 0; i < destinasi[key].length; i++) {
+//             if (destinasi[key][i].id == buttonId) {
+//                 if (pilihan[buttonId] == undefined) {
+//                     pilihan[buttonId] = 0
+//                 }
+//                 pilihan[buttonId]++
+//             }
+//         }
+//     }
+//     generateCard(pilihan)
+//     console.log(pilihan)
+// }
 
-/*
-Pilihan = 
-{
-1: 2,
-2: 1
-}
-*/
+// /*
+// Pilihan = 
+// {
+// 1: 2,
+// 2: 1
+// }
+// */
 
-function generateCard(data) {
-    productCard.innerHTML = ''
-    for (let key in data) {
-        if (data[key] > 0) {
-        // console.log(key, 'key')
-        for (let kota in destinasi) {
-            // console.log(kota, 'kota')
-            for (let i = 0; i < destinasi[kota].length; i++) {
-                if (key == destinasi[kota][i].id) {
-                    console.log(key)
-                    console.log(destinasi[kota][i].id)
-                    let divCard = document.createElement('div')
-                    divCard.setAttribute('divCard', key)
-                    divCard.className = 'card'
+// function generateCard(data) {
+//     productCard.innerHTML = ''
+//     for (let key in data) {
+//         if (data[key] > 0) {
+//         // console.log(key, 'key')
+//         for (let kota in destinasi) {
+//             // console.log(kota, 'kota')
+//             for (let i = 0; i < destinasi[kota].length; i++) {
+//                 if (key == destinasi[kota][i].id) {
+//                     console.log(key)
+//                     console.log(destinasi[kota][i].id)
+//                     let divCard = document.createElement('div')
+//                     divCard.setAttribute('divCard', key)
+//                     divCard.className = 'card'
 
-                    let divImage = document.createElement('div')
-                    divImage.classList.add('cardImg')
-                    divCard.appendChild(divImage)
+//                     let divImage = document.createElement('div')
+//                     divImage.classList.add('cardImg')
+//                     divCard.appendChild(divImage)
 
-                    let imgPackage = document.createElement('img')
-                    imgPackage.src = destinasi[kota][i].Gambar
-                    divImage.appendChild(imgPackage)
+//                     let imgPackage = document.createElement('img')
+//                     imgPackage.src = destinasi[kota][i].Gambar
+//                     divImage.appendChild(imgPackage)
 
-                    let divDetails = document.createElement('div')
-                    divDetails.classList.add('cardDetails')
-                    divCard.appendChild(divDetails)
+//                     let divDetails = document.createElement('div')
+//                     divDetails.classList.add('cardDetails')
+//                     divCard.appendChild(divDetails)
                     
-                    let cityName = document.createElement('h2')
-                    cityName.innerHTML = kota
-                    divDetails.appendChild(cityName)
+//                     let cityName = document.createElement('h2')
+//                     cityName.innerHTML = kota
+//                     divDetails.appendChild(cityName)
 
-                    let packageName = document.createElement('h3')
-                    packageName.innerHTML = `Paket Wisata ${destinasi[kota][i].paket}`
-                    divDetails.appendChild(packageName)
+//                     let packageName = document.createElement('h3')
+//                     packageName.innerHTML = `Paket Wisata ${destinasi[kota][i].paket}`
+//                     divDetails.appendChild(packageName)
 
-                    let termasuk = document.createElement('ul')
-                        for (let j = 0; j < destinasi[kota][i].privilage.length; j++) {
-                            let manfaat = destinasi[kota][i].privilage[j]
-                            var li = document.createElement("li");
-                            var text = document.createTextNode(manfaat);
-                            li.appendChild(text);
-                            termasuk.appendChild(li);
-                        }
+//                     let termasuk = document.createElement('ul')
+//                         for (let j = 0; j < destinasi[kota][i].privilage.length; j++) {
+//                             let manfaat = destinasi[kota][i].privilage[j]
+//                             var li = document.createElement("li");
+//                             var text = document.createTextNode(manfaat);
+//                             li.appendChild(text);
+//                             termasuk.appendChild(li);
+//                         }
 
-                    divDetails.appendChild(termasuk)
+//                     divDetails.appendChild(termasuk)
 
-                    let harga = document.createElement('h3')
-                    harga.className = 'harga'
-                    let jumlahMalam = data[key]
-                    let total = (destinasi[kota][i].Harga * jumlahMalam)
-                    harga.innerHTML = `Rp ${total}`
-                    divDetails.appendChild(harga)
+//                     let harga = document.createElement('h3')
+//                     harga.className = 'harga'
+//                     let jumlahMalam = data[key]
+//                     let total = (destinasi[kota][i].Harga * jumlahMalam)
+//                     harga.innerHTML = `Rp ${total}`
+//                     divDetails.appendChild(harga)
 
-                    let malam = document.createElement('p')
-                    malam.innerHTML = `Jumlah: ${jumlahMalam} malam`
-                    divDetails.appendChild(malam)
+//                     let malam = document.createElement('p')
+//                     malam.innerHTML = `Jumlah: ${jumlahMalam} malam`
+//                     divDetails.appendChild(malam)
 
-                    let button = document.createElement('button')
-                    button.setAttribute('kurangi', key)
-                    button.className = 'kurangi'
-                    button.innerHTML = 'Kurangi'
-                    button.addEventListener('click',remove)
+//                     let button = document.createElement('button')
+//                     button.setAttribute('kurangi', key)
+//                     button.className = 'kurangi'
+//                     button.innerHTML = 'Kurangi'
+//                     button.addEventListener('click',remove)
 
-                    let kurangiAlert = document.createElement('footer')
+//                     let kurangiAlert = document.createElement('footer')
 
-                        function remove() {
-                            if (jumlahMalam > 1) {
-                            harga.innerHTML = ''
-                            malam.innerHTML = ''
-                            jumlahMalam -= 1
-                            total = (destinasi[kota][i].Harga * jumlahMalam)
-                            harga.innerHTML = `Rp ${total}`
-                            malam.innerHTML = `Jumlah: ${jumlahMalam} malam`
-                            } else {
-                                kurangiAlert.className = 'kurangiAlert'
-                                kurangiAlert.innerHTML = 'Jumlah Minimal 1 Malam'
-                                divDetails.appendChild(kurangiAlert)
-                            }
-                        }
-                    divDetails.appendChild(button)
+//                         function remove() {
+//                             if (jumlahMalam > 1) {
+//                             harga.innerHTML = ''
+//                             malam.innerHTML = ''
+//                             jumlahMalam -= 1
+//                             total = (destinasi[kota][i].Harga * jumlahMalam)
+//                             harga.innerHTML = `Rp ${total}`
+//                             malam.innerHTML = `Jumlah: ${jumlahMalam} malam`
+//                             } else {
+//                                 kurangiAlert.className = 'kurangiAlert'
+//                                 kurangiAlert.innerHTML = 'Jumlah Minimal 1 Malam'
+//                                 divDetails.appendChild(kurangiAlert)
+//                             }
+//                         }
+//                     divDetails.appendChild(button)
 
-                    let buttonDelete = document.createElement('button')
-                    buttonDelete.setAttribute('deleteCard', key)
-                    buttonDelete.className = 'delete'
-                    buttonDelete.innerHTML = 'Hapus Produk'
-                    buttonDelete.addEventListener('click',deleteData)
-                        function deleteData() {
-                            divCard.remove()
-                            pilihan[key] = 0
-                            if (productCard.childNodes.length == 0) {
-                                productCard.innerHTML = 'Yah, Keranjangnya Kosong. Kamu Belum Pilih Paket Apapun'
-                            }
-                        }
-                    divDetails.appendChild(buttonDelete)
+//                     let buttonDelete = document.createElement('button')
+//                     buttonDelete.setAttribute('deleteCard', key)
+//                     buttonDelete.className = 'delete'
+//                     buttonDelete.innerHTML = 'Hapus Produk'
+//                     buttonDelete.addEventListener('click',deleteData)
+//                         function deleteData() {
+//                             divCard.remove()
+//                             pilihan[key] = 0
+//                             if (productCard.childNodes.length == 0) {
+//                                 productCard.innerHTML = 'Yah, Keranjangnya Kosong. Kamu Belum Pilih Paket Apapun'
+//                             }
+//                         }
+//                     divDetails.appendChild(buttonDelete)
 
-                    productCard.appendChild(divCard)
-                }
-            }
-        }
-    }
-    }
-}
+//                     productCard.appendChild(divCard)
+//                 }
+//             }
+//         }
+//     }
+//     }
+// }
 
--- script.js
+++ script.js
@@ -50,10 +50,116 @@ let destinasi = {
 const cart = document.getElementById('cart')
 
 let konten = document.getElementById('contentId')
-let kontenClass = document.getElementsByClassName('content')
 
-console.log(konten);
-console.log(kontenClass);
+
+let submitForm = document.getElementById("buttonSubmit");
+
+// submitForm.addEventListener('click', inputed);
+
+function inputed() {
+
+}
+
+
+
+const buttonSubmit = document.getElementById('buttonSubmit')
+buttonSubmit.addEventListener('click', clickButtonSubmit)
+
+function clickButtonSubmit() {
+    konten.innerHTML = "";
+    let inputDestinasi = document.getElementById("tujuan").value
+    let inputBudget = document.getElementById("budget").value
+
+    console.log(generateDestinasi(inputDestinasi, inputBudget));
+
+    generateCart(generateDestinasi(inputDestinasi, inputBudget));
+
+}
+
+
+let inputDestinasi = document.getElementById("tujuan").value
+let inputBudget = document.getElementById("budget").value
+
+// console.log(generateDestinasi(inputDestinasi, inputBudget));
+
+generateCart(generateDestinasi(inputDestinasi, inputBudget));
+
+
+function generateDestinasi(parameterDestinasi, parameterBudget) {
+
+    // parameterDestinasi = document.getElementById("tujuan").value;
+    // parameterBudget = document.getElementById("budget").value;
+
+
+
+    if (parameterDestinasi === '' && parameterBudget === "0") {
+        return `Silahkan masukkan destinasi dan tujuan anda`
+    }
+    if (typeof parameterDestinasi !== 'string') {
+        return `Invalid input`
+    }
+
+    if (isNaN(parameterBudget)) {
+        return "Invalid input";
+    }
+    if (parameterDestinasi === '') {
+        let output = {}
+
+        for (let key in destinasi) {
+            for (let i = 0; i < destinasi[key].length; i++) {
+                let paket = destinasi[key][i]
+                let price = destinasi[key][i]['Harga']
+                if (output[key] === undefined) {
+                    output[key] = []
+                }
+                if (parameterBudget >= price) {
+                    output[key].push(paket)
+                }
+            }
+        }
+        return output
+    }
+
+    if (budget === "0") {
+        let output = {}
+
+        for (let i = 0; i < destinasi[parameterDestinasi].length; i++) {
+            let paket = destinasi[parameterDestinasi][i]
+            if (output[parameterDestinasi] === undefined) {
+                output[parameterDestinasi] = []
+            }
+            output[parameterDestinasi].push(paket)
+        }
+        return output;
+    }
+
+
+    if (parameterDestinasi === 'Jakarta' || parameterDestinasi === 'Bandung') {
+        let output = {}
+
+        for (let key in destinasi) {
+            if (key === parameterDestinasi) {
+                for (let i = 0; i < destinasi[key].length; i++) {
+                    let paket = destinasi[key][i]
+                    let price = destinasi[key][i]['Harga']
+                    if (output[key] === undefined) {
+                        output[key] = []
+                    }
+                    if (Number(parameterBudget) >= price) {
+                        output[key].push(paket)
+                    }
+                }
+            }
+        }
+        return output
+    }
+    if (parameterDestinasi !== 'Jakarta' || parameterDestinasi !== 'Bandung') {
+        return `Mohon maaf destinasi anda belum ada dipaket kami`
+    }
+}
+
+
+
 
 function generateCart(destinasi) {
     // let location = document.getElementById("destinasi").innerHTML;
@@ -85,7 +191,7 @@ function generateCart(destinasi) {
             divDetail.appendChild(h2);
 
             let h3 = document.createElement("h3")
-            h3.innerHTML = "Rp." + destinasi[key][i]["Harga"];
+            h3.innerHTML = "Rp." + Number(destinasi[key][i]["Harga"]).toLocaleString("id-ID");
             divDetail.appendChild(h3);
 
 
@@ -96,52 +202,73 @@ function generateCart(destinasi) {
                 li.innerHTML = destinasi[key][i]["privilage"][j]
                 ol.appendChild(li)
             }
+
+
             divDetail.appendChild(ol)
+
+            let divButtons = document.createElement('div')
+            divButtons.classList.add('buttons')
+
+            let button = document.createElement('button')
+            button.addEventListener('click', Add)
+            button.setAttribute('idTest', i)
+            button.innerHTML = "Add"
+            divButtons.appendChild(button)
+
             // console.log(divCard);
             konten.appendChild(divCard);
+
+
+            konten.appendChild(divButtons);
         }
     }
 
 }
 
-generateCart(destinasi);
 
 
-function addButton() {
-    // document.getElementById("test").innerHTML = "testasfd";
-    let location = document.getElementById("destinasi").innerHTML;
-    cart.innerHTML = "";
 
-    let divCard = document.createElement('div')
-    divCard.classList.add('card')
 
-    let imageLocation = document.createElement('img')
-    imageLocation.classList.add("center")
-    imageLocation.src = destinasi[location][0]["Gambar"];
-    divCard.appendChild(imageLocation)
 
-    let divDetail = document.createElement('div')
-    divDetail.classList.add('detail')
-    divCard.appendChild(divDetail)
 
-    let h2 = document.createElement("h2")
-    h2.innerHTML = destinasi[location][0]["paket"];
-    divDetail.appendChild(h2);
+// function addButton() {
+//     // document.getElementById("test").innerHTML = "testasfd";
+//     let location = document.getElementById("destinasi").innerHTML;
+//     cart.innerHTML = "";
 
-    let h3 = document.createElement("h3")
-    h3.innerHTML = "Rp." + destinasi[location][0]["Harga"];
-    divDetail.appendChild(h3);
+//     let divCard = document.createElement('div')
+//     divCard.classList.add('card')
 
-    let ol = document.createElement("OL")
+//     let imageLocation = document.createElement('img')
+//     imageLocation.classList.add("center")
+//     imageLocation.src = destinasi[location][0]["Gambar"];
+//     divCard.appendChild(imageLocation)
 
-    for (let i = 0; i < destinasi[location][0]["privilage"].length; i++) {
-        let li = document.createElement("LI");
-        li.innerHTML = destinasi[location][0]["privilage"][i]
-        ol.appendChild(li)
-    }
-    divDetail.appendChild(ol)
+//     let divDetail = document.createElement('div')
+//     divDetail.classList.add('detail')
+//     divCard.appendChild(divDetail)
 
-    cart.appendChild(divCard);
-    // console.log(divCard);
+//     let h2 = document.createElement("h2")
+//     h2.innerHTML = destinasi[location][0]["paket"];
+//     divDetail.appendChild(h2);
+
+//     let h3 = document.createElement("h3")
+//     h3.innerHTML = "Rp." + destinasi[location][0]["Harga"];
+//     divDetail.appendChild(h3);
+
+//     let ol = document.createElement("OL")
+
+//     for (let i = 0; i < destinasi[location][0]["privilage"].length; i++) {
+//         let li = document.createElement("LI");
+//         li.innerHTML = destinasi[location][0]["privilage"][i]
+//         ol.appendChild(li)
+//     }
+//     divDetail.appendChild(ol)
+
+//     cart.appendChild(divCard);
+//     // console.log(divCard);
+// }
+
+function Add() {
+    console.log("test");
 }
-
